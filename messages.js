41528707579c25716613fae2ce1905f436044ca3
MiKL-B <michaelbecquer@yahoo.fr>
2024-12-02 17:41:12 +0100

    initial commit

---
+++ messages.js
@@ -0,0 +1,62 @@
+export const messages = {
+  en: {
+    // welcome
+    welcome: `Welcome to my terminal portfolio.<br>
+      For a list of available commands, type 'help'.`,
+    // about
+    about: `Je suis un Développeur web junior passionné par les
+    technologies frontend. Ayant acquis des compétences en
+    HTML, CSS, Javascript et sur des frameworks tels que Vue.js,
+    je suis motivé et déterminé. Mon souhait serait de
+    contribuer à la conception et au développement de projets
+    web innovants au sein d'une équipe dynamique
+    expérimentée.`,
+    name:"Michaël BECQUER",
+    interests:`Insterets:<br>Lecture<br>Peinture`,
+    // contact
+
+    // experience
+    jobTitle1:"Analyste programmeur",
+    jobPeriod1:"De septembre 2022 à novembre 2024",
+    jobCompany1:"Compusoft / Lunéville",
+    jobTitle2:"Analyste programmeur stagiaire",
+    jobPeriod2:"De mai 2022 à juillet 2022",
+    jobTitle3:"Développeur web stagiaire",
+    jobPeriod3:"De mars 2021 à mai 2021",
+    jobCompany3:"Association Kyosai / Télétravail",
+    // formation
+    // skills
+    // projects
+
+    // language
+    languages:"Languages:<br>French<br>English",
+    // help
+
+    // error
+    error: "[Error]: Command not found!",
+  },
+  fr: {
+    // welcome
+    welcome: `Welcome to my terminal portfolio.<br>
+       For a list of available commands, type 'help'.`,
+    // about
+    about: `Je suis un Développeur web junior passionné par les
+technologies frontend. Ayant acquis des compétences en
+HTML, CSS, Javascript et sur des frameworks tels que Vue.js,
+je suis motivé et déterminé. Mon souhait serait de
+contribuer à la conception et au développement de projets
+web innovants au sein d'une équipe dynamique
+expérimentée`,
+    // contact
+    // experience
+    // formation
+    // skills
+    // projects
+    // language
+    // help
+  },
+};
+
+// let local = "en"
+// createMessage(key)
+// messages[local][key]
